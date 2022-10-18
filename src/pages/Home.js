b5d6e42b4ef8c0fa4ae25f1a45ffc43f87f45727
Beatriz Nonato <bia_nonato@icloud.com>
2022-10-18 18:13:01 -0300

    refactor: change font family, add margin and add component CustomerList in Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 import SlideShow from '../components/SlideShow/SlideShow';
+import CustomerList from '../components/CustomerList/CustomerList';
+import './Home.css'
 
 function Home() {
     return (
         <>
         <Header />
-        <SlideShow />
+        <main className='main__container'>
+             <SlideShow />
+             <CustomerList />
+        </main>
         <Footer />
         </>
     )
