e5886033b76489f7f633ae673e48f764e3a366cd
Beatriz Nonato <bia_nonato@icloud.com>
2022-10-18 20:21:10 -0300

    refactor: add content h5 and paragraph

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,8 @@ function Home() {
         <main className='main__container'>
              <SlideShow />
              <CustomerList />
+             <h5>Sobre</h5>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur odio quam, sed dignissim enim pharetra sagittis. Ut ultrices venenatis magna, eu vulputate enim rhoncus at. Cras sollicitudin facilisis mi, quis congue leo porta a. Morbi fermentum eleifend lectus in eleifend.</p>
         </main>
         <Footer />
         </>
