2cee6d8436eaae2b75def2deaf6fba7bc4a3c525
Beatriz Nonato <bia_nonato@icloud.com>
2022-10-18 15:24:27 -0300

    refactor: add slideshow component in Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import SlideShow from '../components/SlideShow/SlideShow';
 
 function Home() {
     return (
+        <>
         <Header />
+        <SlideShow />
+        <Footer />
+        </>
     )
 }
 
