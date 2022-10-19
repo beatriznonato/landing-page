f4f0d80a4cf5777a3cf94712189f0c8559de7e7f
Beatriz Nonato <bia_nonato@icloud.com>
2022-10-19 03:42:54 -0300

    refactor: change const images, zoom for slide and add customArrown and prev/next arrow
    n

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import { Zoom } from 'react-slideshow-image';
+import { Slide } from 'react-slideshow-image';
 import banner from '../../assets/images/banner.png'
 import './SlideShow.css'
 
-const images = [`${banner}`, `${banner}`, `${banner}`];
+const images = [
+  {
+    url: banner,
+    title: 'Slide 1'
+  },
+  {
+    url: banner,
+    title: 'Slide 2'
+  },
+  {
+    url: banner,
+    title: 'Slide 3'
+  },
+];
 
-const zoomOutProperties = {
-    infinite: true,
-    indicators: true,
-    scale: 0.4,
-    arrows: true
+const customArrow = <button>{'>'}</button>
+
+const slideProperties = {
+  infinite: true,
+  indicators: true,
+  scale: 0.4,
+  arrows: true,
+  prevArrow: customArrow,
+  nextArrow: customArrow
 };
 
-  
 const SlideShow = () => {
-    return (
-      <div className="slide__container">
-        <Zoom {...zoomOutProperties} className='slide__zoom'>
-          {images.map((each, index) => (
-            <img className='slide__image' key={index} src={each} />
+  return (
+    <div className="slide__container">
+      <Slide {...slideProperties}>
+        {images.map((slideImage, index) => (
+          <img alt='banner "Bom pra Caju"' className='slide__image' key={index} src={slideImage.url} title={slideImage.title}
+            />
           ))}
-        </Zoom>
-      </div>
-    );
-  };
+      </Slide>
+    </div>
+  );
+};
 
 export default SlideShow;
